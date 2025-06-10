3d0d47cb9e205f67c3b5c46be89c1cd69bdfe695
chirag0631 <chiru7975@gmail.com>
2025-06-10 14:40:15 +0530

    card has been added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,30 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { ChevronRight } from "lucide-react";
+import { BarChart, ChevronRight } from "lucide-react";
 import Link from "next/link";
 import { TypeAnimation } from 'react-type-animation';
+import { Layout, Calendar } from "lucide-react";
 import React from "react";
+import { Card,CardContent } from "@/components/ui/card";
+
+const features = [
+  {
+    title: "Intuitive Dashboard",
+    description: "Get a clear overview of your projects, sprints, and tasks with our easy-to-use dashboard.",
+    icon: Layout,
+  },
+  {
+    title: "Sprint Planning",
+    description: "Effortlessly schedule and manage your sprints with an integrated calendar and timeline view.",
+    icon: Calendar,
+  },
+  {
+    title: "Progress Tracking",
+    description: "Visualize team progress and productivity using dynamic charts and performance metrics.",
+    icon: BarChart,
+  },
+];
 
 export default function Home() {
   return (
@@ -43,6 +63,27 @@ export default function Home() {
             
           </Button>
         </Link>
+      </section>
+
+      <section id="features" className="bg-gray-900 py-20 px-5">
+        <div className="container mx-auto">
+        <h3 className="text-3xl font-bold mb-12 text-center">Key Features</h3>
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
+          {features.map((feature, index) => {
+            return(
+              <Card key={index} className="bg-gray-800 hover:bg-gray-600">
+                <CardContent className="pt-6">
+                  <feature.icon className="h-12 w-12 mb-4 text-blue-300"/>
+                  <h4 className="text-xl font-semibold mb-2">{feature.title}</h4>
+                  <p className="text-gray-300">{feature.description}</p>
+                </CardContent>
+              </Card>
+            )
+          })}
+          
+          
+        </div>
+        </div>
       </section>
     </div>
   );
